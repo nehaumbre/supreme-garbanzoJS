beab308291e9a6f502932b9fb5e88148748c56da
nehaumbre <nehaumbre28@gmail.com>
2025-06-22 20:18:51 +0530

    Initial Commit: Basic Variables and consolelog
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

---
+++ Basic/BasicJS/app.js
@@ -0,0 +1,15 @@
+// console.log("Hello");
+// console.error("This is an error");
+// console.warn("This is a warning");
+// console.table({name: "jun", age: 20});
+
+// 1. Create variable name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+let name = "Neha";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let twitterHandle ="SCABOOM"
+console.log("Hi My Name is " + name + " I want to be a " + gender + " " + whatDoYouWannaBecomeInYourLife + " My twitter handle is " + twitterHandle);
