f14407550c81414f34e570d7249a6201a4237f55
nehaumbre <nehaumbre28@gmail.com>
2025-06-24 14:39:03 +0530

    Initial Commit: Basic JS
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

---
+++ Basic/TypeConversion/app.js
@@ -0,0 +1,8 @@
+// - Convert string to number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount)
+
+//  - Convert number to string
+// money = money.toString();
+// money = String(money);
---
+++ Basic/operators/app.js
@@ -0,0 +1,34 @@
+// 1. Create variable name (firstFavNumb) & store your favorite number.
+// 2. Create variable name (secondFavNumb) & store second fav Number.
+// 3. Check (firstFavNumb is greater then secondFavNumb)
+// 4. Check (firstFavNumb is less then secondFavNumb)
+// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
+// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
+// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
+// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
+// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
+
+let firstFavNumb = 12;
+let secondFavNumb = 7;
+
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
+
+// > Greater Then
+// < Less Then
+// >= Greater Then or equal to
+// <= Less Then or equal to
+
+// ***********************************
+// EQUALITY OPERATORS
+// === strict equality (Type + Value)
+// !== strict non-equality (Type + Value)
+// == Lose equality (values)
+// != Lose equality (values)
