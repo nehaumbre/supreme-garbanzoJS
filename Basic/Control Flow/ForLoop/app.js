7d15ee8a9b8b36ea1a64be072d0abb4ad5c61914
nehaumbre <nehaumbre28@gmail.com>
2025-06-25 15:33:45 +0530

    Control Flow
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

---
+++ Basic/Control Flow/ForLoop/app.js
@@ -0,0 +1,15 @@
+// for (initialExpression; condition; incrementExpression) {}
+let array = [1, 3, 6, 5];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+for (index = 0; index < 5; index++) {
+    console.log("hi" + index);
+}
+
+// Print your name & numbers from 0 through 1000
+for (index = 0; index <= 1000; index++) {
+    console.log("I love you " + index);
+}
