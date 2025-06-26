d45119f1a6bd939b46400abc1b47ac88619e3fdf
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-06-26 15:34:14 +0530

    Logical ops
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- Basic/Control Flow/Logical Operators/apps.js
+++ Basic/Control Flow/Logical Operators/apps.js
@@ -1,8 +1,15 @@
 // -----------------------------------
 // 1. Logical AND (&&)
 //  -- TRUE if both the operands/boolean values are true, else evaluates to FALSE
+const a = true;
+const b = false;
+const c = 4;
+console.log(a && b);
 // -----------------------------------
 // 2. Logical OR (||)
 // -- TRUE if either of the operands/boolean values is true. evaluates to false if both are FALSE
+console.log(a || b);
 // 3. Logical NOT (!)
 // TRUE if the operand is false and vice-versa.
+console.log(!a);
+console.log(!b);
