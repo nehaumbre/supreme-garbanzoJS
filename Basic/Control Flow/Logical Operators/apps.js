c52ada2d0618d3f88d418508cf46bbd5026e7386
nehaumbre <nehaumbre28@gmail.com>
2025-06-26 15:04:12 +0530

    Control Flow
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

---
+++ Basic/Control Flow/Logical Operators/apps.js
@@ -0,0 +1,8 @@
+// -----------------------------------
+// 1. Logical AND (&&)
+//  -- TRUE if both the operands/boolean values are true, else evaluates to FALSE
+// -----------------------------------
+// 2. Logical OR (||)
+// -- TRUE if either of the operands/boolean values is true. evaluates to false if both are FALSE
+// 3. Logical NOT (!)
+// TRUE if the operand is false and vice-versa.
---
+++ Basic/Control Flow/WhileandDowhile/app.js
@@ -0,0 +1,22 @@
+// let i = 0;
+// while (i <= 5) {
+//     console.log("Hi", i);
+//     i++;
+// }
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+let i = 10;
+while (i <= 100) {
+    console.log("while", i);
+    i++;
+}
+
+// Do While
+// Print "Your Name" 400 times to the console using do-while-loop.
+// Iteration should start from 20.
+let j =20;
+do {
+    console.log("Do while", j);
+    j++;
+} while (j<=400);
+
