5401b97de88a1404420f581759815b27348cae2a
nehaumbre <nehaumbre28@gmail.com>
2025-06-28 15:01:03 +0530

    methods in Js
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- Basic/Functions/scopes_and_methods/scope.js
+++ Basic/Functions/scopes_and_methods/scope.js
@@ -5,13 +5,13 @@
 // Global variables are those declared outside of a block.
 
 // Local variables are those declared inside of a block.
-let textMessage ="hello"; //Global
-console.log(textMessage); 
+let textMessage = "hello"; //Global
+console.log(textMessage);
 
 function showMessage() {
     let textMessage = "Hi!"; //local
     console.log(textMessage); //access local scope
-}  
+}
 showMessage();
 
 
@@ -20,3 +20,23 @@ for (let index = 0; index < 6; index++) {
 }
 
 // console.log(index); //global scope  wont work because we access local var outside its scope
+
+
+//Methods are functions inside an object
+
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2022,
+    color: "blue",
+    start: function () {
+        console.log("start the car");
+    },
+    drive: function (speed) {
+        console.log(`the speed is ${speed}km/h`);
+    }
+}
+
+console.log(car.start);
+console.log(car.start());
+console.log(car.drive(90));
