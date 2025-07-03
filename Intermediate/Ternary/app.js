1c0f0fd6bfe843072f6ce7edf43726d583c5da9c
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-03 23:36:20 +0530

    Ternary Operator
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

---
+++ Intermediate/Ternary/app.js
@@ -0,0 +1,7 @@
+// The ternary operator in JavaScript is a concise way of writing conditional expressions.
+// It is the only JavaScript operator that takes three operands: 
+//a condition followed by a question mark (?), an expression to execute if the condition is true, 
+//followed by a colon (:), and an expression to execute if the condition is false.
+
+// condition ? exprIfTrue : exprIfFalse;
+
