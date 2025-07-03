5e4c283d04ba7c5cdedc16a21a17343e2934ac80
nehaumbre <nehaumbre28@gmail.com>
2025-07-03 20:42:05 +0530

    DESTRUCTURING object
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- Intermediate/Destructuring/app.js
+++ Intermediate/Destructuring/app.js
@@ -50,3 +50,21 @@ const [color1, color2, color3] = colors;
 console.log(color1);
 console.log(color2);
 console.log(color3);
+
+//*object destructuring 
+let student = { name: "Jun", position: "First", rollNo: 45, grade: "four" }
+const { name: fullname, position, rollNo ,grade} = student; //*renaming values
+console.log(fullname, position, rollNo, grade);
+
+const person = {
+    name: "John Doe",
+    age: 30,
+    gender: "male",
+    country: "USA",
+};
+
+// Your task is to use object destructuring to extract the name, age, and country properties from the person object.
+  // After extracting the properties, log each variable's value to the console.
+
+const {name,age,country} = person;
+console.log(name,age,country);
