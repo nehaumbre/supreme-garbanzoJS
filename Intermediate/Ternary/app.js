7d890c818eb2b0e44e1e7f15a7e1ddd239128eb3
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-04 15:49:12 +0530

    Ternary Operator
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- Intermediate/Ternary/app.js
+++ Intermediate/Ternary/app.js
@@ -5,3 +5,45 @@
 
 // condition ? exprIfTrue : exprIfFalse;
 
+// if (condition) {
+    
+// }
+// else{
+
+// }
+
+let password = 8;
+
+function passwordChecker(ps){
+    // if (ps === 8){
+    //     return `strong password`
+    // }else{
+    //     return `weak password! password should be 8 char.`
+    // }
+    return ps===8 ? `strong password` : "weak password! password should be 8 char."
+}
+
+const res = passwordChecker(password);
+console.log(res);
+
+
+// -------------------------------------
+// REFACTOR THIS CODE & USE TERNARY OPERATOR
+// check the age to determine the eligibility to vote
+let age = 1;
+// let result;
+
+// if (age >= 18) {
+//       result = "You are eligible to vote.";
+// } else {
+//       result = "You are not eligible to vote yet.";
+// }
+let result = age >= 18 ? "You are eligible to vote" :`You are not eligible to vote yet.`;
+
+console.log(result);
+
+// Create a program which checks if number is positive, negative or zero
+
+let number = 1;
+let answer = number>= 0 ? (number == 0 ? "number is zero": "number is positive") : "number is negative";
+console.log(answer);
