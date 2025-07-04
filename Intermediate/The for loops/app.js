bde11a874d29832066401891377d597764d0b151
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-04 16:53:30 +0530

    for o loop
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- Intermediate/The for loops/app.js
+++ Intermediate/The for loops/app.js
@@ -47,11 +47,37 @@ let total = 0;
 let count = 0;
 for (let scores in testScores) {
   total = total + testScores[scores]; // or use total += scores[subject];
-  count ++
+  count++;
 }
 console.table(testScores);
-console.log("Total MArks",total);
+console.log("Total MArks", total);
 console.log("No of subjects", count);
-console.log("Average is", total/count);
+console.log("Average is", total / count);
 
+// for of loop
+// The "for...of" loop in JavaScript is a modern iteration statement introduced in ECMAScript 6 (ES6)
+//that provides a concise and easy way to loop over elements in iterable objects like arrays, strings, maps, sets, and more.
+//It allows you to iterate directly over the values of the elements, rather than dealing with their indices or keys,
+// which makes the code more readable and less error-prone.
 
+// for (variable of iterable) {...}
+
+// Here, variable is a variable that represents each value of the iterable during each iteration, and iterable is the iterable object being looped through.
+let peoples = ["huxn", "alex", "john", "brad"];
+console.table(peoples);
+for (let people of peoples) {
+  console.log(people);
+}
+// Iterating over String
+const text = "Heeellello";
+
+for (const char of text) {
+  console.log(char);
+}
+
+// Iterate over "array1" & log the values using for of loop.
+const array1 = ["a", "b", "c"];
+
+for (let chars of array1) {
+  console.log(chars);
+}
