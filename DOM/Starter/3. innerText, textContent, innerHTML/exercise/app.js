c0ac449283502b8d2ebe6b97260f5202d4cb6de8
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-06 14:45:43 +0530

    DOM : innerText,textContent,innerHRTML
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/3. innerText, textContent, innerHTML/exercise/app.js
+++ DOM/Starter/3. innerText, textContent, innerHTML/exercise/app.js
@@ -1,3 +1,13 @@
 // 1. Check the innerText of "first" element
 // 2. Check the textContent of "second" element
 // 3. Check the innerHTML of "third" element
+
+let innerTextElementOne = document.querySelector('.first').innerText;
+console.log(`Check the innerText of "first" element : 
+              ${innerTextElementOne}`);
+
+let textContentElementTwo = document.querySelector('.second').textContent;
+console.log(`Check the textContent of "second" element : ${textContentElementTwo}`);
+
+let innerHTMLElementThree = document.querySelector('.third').innerHTML;
+console.log(`Check the innerHTML of "third" element : ${innerHTMLElementThree}`);
--- DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
+++ DOM/Starter/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,15 @@
 // innerText
 // textContent
 // innerHTML
+let innerTextOfP = document.querySelector("p").innerText;
+console.log(`OutPut of innerText`);
+console.log(innerTextOfP);
+let innerHTMLOfP = document.querySelector("p").innerHTML;
+console.log(`OutPut of innerHTML`);
+console.log(innerHTMLOfP);
+let textContent = document.querySelector("p").textContent;
+console.log(`OutPut of textContent`);
+console.log(textContent);
+
+document.querySelector("h1").innerText = "Bye World!";
+document.querySelector("h1").innerHTML = "<em>Bye!!</em> World";
