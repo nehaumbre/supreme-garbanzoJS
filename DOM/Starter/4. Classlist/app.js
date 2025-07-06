87a16dceaf0fcb01517129a34d7b66e1927b625b
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-06 15:10:41 +0530

    DOM : innerText,textContent,innerHRTML
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/4. Classlist/app.js
+++ DOM/Starter/4. Classlist/app.js
@@ -1,4 +1,14 @@
 // classlist
 // add()
 // remove()
-// toggle()
+// toggle() = class nahi hain to add krega and hain to remove krega
+
+let classListOfh1 = document.querySelector('h1').classList;
+console.log(classListOfh1);
+
+document.querySelector('h1').classList.add("noshow");
+// document.querySelector('h1').classList.remove("noshow");
+// document.querySelector('h1').classList.toggle("noshow");
+// add() method adds one or more classes to an element
+// remove() method removes one or more classes from an element
+// toggle() method toggles one or more classes on an element
