eeb4008c3f6b106473cf92814b5d3c39532aa018
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-08 12:34:37 +0530

    DOM : get and set attributes
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
+++ DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -7,21 +7,26 @@
 const a = document.querySelector("a");
 console.log(a);
 console.log(a.href);
-console.log((a.href = "https://www.youtube.com/watch?v=H3XIJYEPdus&list=PLSDeUiTMfxW4zCLgOQgz4PWSN0QRmUUFR"));
-const input = document.querySelector('input');
+console.log(
+  (a.href =
+    "https://www.youtube.com/watch?v=H3XIJYEPdus&list=PLSDeUiTMfxW4zCLgOQgz4PWSN0QRmUUFR")
+);
+const input = document.querySelector("input");
 console.log(input);
 console.log(input.value);
 console.log(input.type);
 
-const input1 = document.querySelector('#password');
-console.log(input1.value =" Enter strong password")
+const input1 = document.querySelector("#password");
+console.log((input1.value = " Enter strong password"));
 //Show password checkbox code
-const passwordInput = document.getElementById('password');
-const showPasswordCheckBox = document.querySelector('#showPassword')
+const passwordInput = document.getElementById("password");
+const showPasswordCheckBox = document.querySelector("#showPassword");
 
-showPasswordCheckBox.addEventListener('change', function () {
-    passwordInput.type = this.checked ? "text" :"password";
+showPasswordCheckBox.addEventListener("change", function () {
+  passwordInput.type = this.checked ? "text" : "password";
 });
 
-
 //Getting Attributes
+console.clear();
+console.log(input.getAttribute("type"));
+input.setAttribute("required", "true");
