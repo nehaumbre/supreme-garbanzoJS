475b5a4100e4aa2bdf46a69fbbb4e12d89da0f4a
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-08 12:17:55 +0530

    DOM : show password checkbox
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
+++ DOM/Starter/5 Getting And Setting Attributes/live coding/app.js
@@ -4,7 +4,24 @@
 // getAttribute(attrName)
 // setAttribute(attrName, value)
 
-const a  = document.querySelector('a');
+const a = document.querySelector("a");
 console.log(a);
 console.log(a.href);
-MyNewHue22@#
+console.log((a.href = "https://www.youtube.com/watch?v=H3XIJYEPdus&list=PLSDeUiTMfxW4zCLgOQgz4PWSN0QRmUUFR"));
+const input = document.querySelector('input');
+console.log(input);
+console.log(input.value);
+console.log(input.type);
+
+const input1 = document.querySelector('#password');
+console.log(input1.value =" Enter strong password")
+//Show password checkbox code
+const passwordInput = document.getElementById('password');
+const showPasswordCheckBox = document.querySelector('#showPassword')
+
+showPasswordCheckBox.addEventListener('change', function () {
+    passwordInput.type = this.checked ? "text" :"password";
+});
+
+
+//Getting Attributes
