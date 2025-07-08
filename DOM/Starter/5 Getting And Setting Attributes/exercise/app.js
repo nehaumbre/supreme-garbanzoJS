8b8d44e53c0dfc1096168d377678a9b4f54288d2
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-08 12:43:04 +0530

    DOM : get and set attributes
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
+++ DOM/Starter/5 Getting And Setting Attributes/exercise/app.js
@@ -2,3 +2,10 @@
 // 2. Use getAttribute(attrName) to check the attribute.
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+
+const anchor = document.querySelector("a");
+const attr = document.querySelector('input').getAttribute('type');
+document.querySelector(".a-two").setAttribute("href", "https://www.udemy.com/");
+
+console.log(anchor.href);
+console.log(attr);
