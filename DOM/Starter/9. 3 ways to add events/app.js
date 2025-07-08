7f9c121b693967f8617025bda245f83e2d47bd37
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-09 01:04:22 +0530

    DOM EVENT
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/9. 3 ways to add events/app.js
+++ DOM/Starter/9. 3 ways to add events/app.js
@@ -1,5 +1,22 @@
 // ----------- BAD WAY-----------
+// const butt = document.querySelector('.second-btn');
+// butt.onclick = function (){
+//     console.log("BBOM");
+// }
 
 // ----------- GREAT WAY-----------
+const best = document.querySelector(".best");
+best.addEventListener("click", function () {
+  console.log("BOOM");
+});
 
 // ----------- Event (e) Object -----------
+const para = document.querySelector(".para");
+para.addEventListener("click", (event) => console.log(event));
+
+let form = document.querySelector("form");
+let inp = document.querySelector('#pas');
+form.addEventListener("click", (event) => { // event can also be written as e
+  event.preventDefault();
+  console.log(inp.value);
+});
