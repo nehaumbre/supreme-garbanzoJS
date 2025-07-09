58d0469f1ad397ee1d92144294ad7f26db19e74e
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-09 12:02:00 +0530

    DOM EVENT: Mouse
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
+++ DOM/Starter/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,17 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+
+// input.addEventListener('keydown', () => {
+//     console.log("Key pressed!");
+// })
+// input.addEventListener('keyup', () => {
+//     console.log("Key up!");
+// })
+
+// input.addEventListener('keydown', e => {
+//     console.log(e.charCode);// deprecated
+// })
+
+input.addEventListener('keydown', e => {
+        console.log(e.code);
+    })
