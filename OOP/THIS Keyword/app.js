2c8172787cf5970d54924c94f4315e2c9bcdd36b
nehaumbre <nehaumbre28@gmail.com>
2025-07-09 15:26:50 +0530

    THIS Keyword
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- OOP/THIS Keyword/app.js
+++ OOP/THIS Keyword/app.js
@@ -50,3 +50,27 @@ let obj = {
 
 let name = obj.fullNme();
 console.log(name);
+
+
+// Create a person object and store your name & age
+// -> Also create a method name (greetRegular) by using a regular function
+//  --> Print this message `Hello, my name is ${this.name} & I'm ${this.age} years old.`
+// -> Create one more method name (greetArrow) using arrow function
+//  --> Print this message `Hello, my name is ${this.name} & I'm ${this.age} years old.`
+
+// *************** SOLUTION
+
+let person = {
+    name: 'Simon',
+    age: 40,
+    greetregular: function () {
+        return `Hello, my name is ${this.name} & I'm ${this.age} years old.`
+    },
+    greetArrow: () => {
+        return `Hello, my name is ${this.name} & I'm ${this.age} years old.`
+    }
+}
+let regular = person.greetregular();
+let arrow = person.greetArrow();
+console.log(regular);
+console.log(arrow);
