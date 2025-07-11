ceb3332fb0aa48ef63541347c2d92255d9507db2
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-11 18:37:37 +0530

    Modifiers
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- OOP/classes/modifiers.js
+++ OOP/classes/modifiers.js
@@ -1,1 +1,53 @@
 console.clear();
+
+// private
+// public
+//protected
+// we dont have above keywords in JS
+//we either use this or closures
+
+//see below constructor function
+function MyClass(publicF, privateF, protectedF) {
+  //Public Field
+  this.publicF = publicF; //accessible globally
+
+  //private Field (closure)
+  const _privateF = privateF; // not accessible outside class
+
+  // protected Field
+  const _protectedF = protectedF;
+
+  // Public method
+  this.publicMethod = function () {
+    return `Public Field: ${this.publicF}`;
+  };
+
+  //private method (closure)
+  function _privateMethod() {
+    return `Private Field: ${_privateF}`;
+  }
+
+  // protected method (closure)
+  function _protectedMethod() {
+    return `Protected Field : ${_protectedF}`;
+  }
+
+  //Method to access protected method
+  this.accessProtectedMethod = function () {
+    return _protectedMethod();
+  };
+  this.accessPrivateMethod = function () {
+    return _privateMethod();
+  };
+}
+
+const myObject = new MyClass("Public Data", "Private Data", "Protected Data");
+
+console.log(myObject.publicF); //Public Data
+console.log(myObject._privateF); //undefined
+console.log(myObject._protectedF); //undefined
+console.log(myObject.publicMethod()); //Public Field: Public Data
+// console.log(myObject._privateMethod());// Error :myObject._privateMethod is not a function
+// console.log(myObject._protectedMethod()); //Error: myObject._protectedMethod is not a function
+console.log(myObject.accessProtectedMethod());
+console.log(myObject.accessPrivateMethod());
