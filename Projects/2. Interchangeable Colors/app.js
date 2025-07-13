3191ac640c63ccb663c12e1688c17d4490567837
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-13 20:08:47 +0530

    DOM : Project2: Interchangeable Website Background Color
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- Projects/2. Interchangeable Colors/app.js
+++ Projects/2. Interchangeable Colors/app.js
@@ -1,8 +1,18 @@
-document.addEventListener("click", () => {
-  let background = event.srcElement.value;
-  document.querySelector("body").style.backgroundColor = background;
-  document.querySelector("." + background).classList.add("clicked");
-  setTimeout(() => {
-    document.querySelector("." + background).classList.remove("clicked");
-  }, 200);
-});
+// document.addEventListener("click", () => {
+//   let background = event.srcElement.value;
+//   document.querySelector("body").style.backgroundColor = background;
+//   document.querySelector("." + background).classList.add("clicked");
+//   setTimeout(() => {
+//     document.querySelector("." + background).classList.remove("clicked");
+//   }, 200);
+// });
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn")){
+    let background = e.target.value;
+    document.querySelector("body").style.backgroundColor = background;
+    document.querySelector("." + background).classList.add("clicked");
+    setTimeout(() => {
+      document.querySelector("." + background).classList.remove("clicked");
+    }, 200);
+  }});
