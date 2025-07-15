b7ee56dae961eaa95a05c8504d1d51cd87a91fc0
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-15 22:28:44 +0530

    Promises NEha version and Aync await
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

--- Sync and Async/Async and Await/app.js
+++ Sync and Async/Async and Await/app.js
@@ -76,22 +76,142 @@ function loginUser(username, password) {
 
 //*Let’s rewrite the same login example using async/await.
 
-// Async function to use await
-async function loginFlow() {
-  console.log("START");
-  try {
-    const result = await loginUser("admin", "1234");
-    console.log("Result:", result);
-  } catch (error) {
-    console.log("Error:", error);
-  }
-  console.log("END");
-}
+// *Async function to use await
+// async function loginFlow() {
+//   console.log("START");
+//   try {
+//     const result = await loginUser("admin", "1234");
+//     console.log("Result:", result);
+//   } catch (error) {
+//     console.log("Error:", error);
+//   }
+//   console.log("END");
+// }
 
-loginFlow();
+// loginFlow();
 
 // console.log("Outside the async function, JavaScript keeps running (non-blocking).");
 // loginUser still returns a Promise.
 // await pauses inside the async function until the promise is resolved or rejected.
 // try...catch is used to handle errors, just like .then().catch().
 // Outside the async function, JavaScript keeps running (non-blocking).
+
+// async/await is built on top of Promises.
+
+// It simplifies asynchronous code and error handling.
+
+// It doesn’t block the program, just pauses the async function.
+
+// It's especially useful in real-world code (e.g., working with APIs).
+
+// ----------------------------------------------
+// Refactor this code to use Promises
+// console.log("Start");
+
+// function getUserDataFromDB(name, callback) {
+//   setTimeout(() => {
+//     console.log("Getting User Name...");
+//     callback(name);
+//   }, 2000);
+// }
+
+// function getUserHobbies(name, callback) {
+//   setTimeout(() => {
+//     console.log("Getting user hobbies...");
+//     callback(["Cricket", "Reading", "Dancing"]);
+//   }, 2000);
+// }
+
+// getUserDataFromDB("HuXn", (data) => {
+//   console.log(data);
+//   getUserHobbies(data, (hobby) => {
+//     console.log(hobby);
+//   });
+// });
+
+// console.log("End");
+// ----------------------------------------------
+
+console.log("Start challenge");
+
+function getUserHobbies(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`name: ${name}`);
+      resolve(name);
+    }, 2000);
+  });
+}
+
+function getUserDataFromDB() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting user hobbies...");
+      resolve(["Cricket", "Reading", "Dancing"]);
+      // reject("Database error");
+    }, 2000);
+  });
+}
+
+// getUserHobbies("Jun")
+// .then((name)=> {
+//     console.log(name);
+//     return getUserDataFromDB(); //returns new promise
+// })
+// .then((hobby) => console.log(hobby))
+// .catch((error) => {
+//     console.error("Something went wrong:", error);
+//   });
+
+async function getDataFlow() {
+  try {
+    const userNamefromData = await getUserHobbies("Jun");
+    const hobbiesfromData = await getUserDataFromDB();
+    console.log(`NAME: ${userNamefromData}`);
+    console.log(`HOBBIES:${hobbiesfromData}`);
+  } catch (error) {
+    console.error("Something went wrong:", error);
+  }
+}
+
+getDataFlow();
+console.log("End Challenge");
+// *OUTPUT in success
+// Start challenge
+// app.js:162 End Challenge
+// app.js:140 name: Jun
+// app.js:157 Jun
+// app.js:149 Getting user hobbies...
+// app.js:160 (3) ['Cricket', 'Reading', 'Dancing']
+//! OUTPUT in error
+//app.js:163  Something went wrong: Database error
+
+function fetchDataFromServer() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`User data recieved from server...`);
+    }, 4000);
+  });
+}
+
+async function getUserData() {
+  try {
+    let userdata = await fetchDataFromServer();
+    console.log(userdata);
+    console.log("REmaining task is executed here..");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUserData();
+
+//* This function:
+
+// Waits for fetchDataFromServer() using await
+
+// Logs the result after it resolves
+
+// Then runs the remaining task
+
+// If an error happens, it’s caught in catch
